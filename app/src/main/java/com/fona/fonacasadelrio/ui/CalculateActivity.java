d5ad7d0821ff6df50eb936311974cedc67aa8c3d
wejdanmurad <annoying0597>
2023-02-07 22:54:05 +0200

    s

--- app/src/main/java/com/fona/fonacasadelrio/ui/CalculateActivity.java
+++ app/src/main/java/com/fona/fonacasadelrio/ui/CalculateActivity.java
@@ -2,12 +2,15 @@ package com.fona.fonacasadelrio.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.fona.fonacasadelrio.MainActivity;
 import com.fona.fonacasadelrio.R;
 import com.fona.fonacasadelrio.databinding.ActivityCalculateBinding;
 
-public class CalculateActivity extends AppCompatActivity {
+public class CalculateActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityCalculateBinding binding;
 
@@ -16,5 +19,12 @@ public class CalculateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityCalculateBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == binding.imageView2.getId()) {
+            startActivity(new Intent(this, MainActivity.class));
+        }
     }
 }
