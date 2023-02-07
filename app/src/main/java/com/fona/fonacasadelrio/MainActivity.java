cec29130b3ea8757a4b74070991b8a62e690b913
SohaibHassan <sh7727313sh>
2023-02-07 20:05:22 +0200

    view binding

--- app/src/main/java/com/fona/fonacasadelrio/MainActivity.java
+++ app/src/main/java/com/fona/fonacasadelrio/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.fona.fonacasadelrio.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
     }
 }
