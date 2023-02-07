a468e87ed8c0867d5ea4b0b55988ca1df0497232
wejdanmurad <annoying0597>
2023-02-07 19:46:58 +0200

    s

--- app/src/main/java/com/fona/fonacasadelrio/SplashActivity.java
+++ app/src/main/java/com/fona/fonacasadelrio/SplashActivity.java
@@ -1,24 +1,23 @@
 package com.fona.fonacasadelrio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
 
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ConstraintLayout layout;
+    static int background = R.drawable.background_gradient_red;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        final int sdk = android.os.Build.VERSION.SDK_INT;
-        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            layout.setBackgroundDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.background_gradient_red, null));
-        } else {
-            layout.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.background_gradient_red, null));
-        }
-        ;
+        layout = findViewById(R.id.constraint_layout);
+
+        layout.setBackground(ResourcesCompat.getDrawable(getResources(), background, null));
     }
 }
